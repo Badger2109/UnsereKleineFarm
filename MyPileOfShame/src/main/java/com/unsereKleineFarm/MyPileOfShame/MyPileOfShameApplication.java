7876221eb21ac4e0c6ef29b35d3045adaa894b61
Sebastian Maxheim <sebastian.maxheim@gmail.com>
2023-01-21 14:12:18 +0100

    Drölfter Commit

---
+++ MyPileOfShame/src/main/java/com/unsereKleineFarm/MyPileOfShame/MyPileOfShameApplication.java
@@ -0,0 +1,13 @@
+package com.unsereKleineFarm.MyPileOfShame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPileOfShameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPileOfShameApplication.class, args);
+	}
+
+}
---
+++ MyPileOfShame/src/test/java/com/unsereKleineFarm/MyPileOfShame/MyPileOfShameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsereKleineFarm.MyPileOfShame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPileOfShameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
